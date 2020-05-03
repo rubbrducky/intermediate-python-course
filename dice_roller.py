53c215afedaa6b66a54ab8ccc5aa92534c05366a
rubbrducky <gustav.algotsson@gmail.com>
2020-05-03 14:46:16 +0200

    so many sides

--- dice_roller.py
+++ dice_roller.py
@@ -2,13 +2,19 @@ import random
   
 def main():
     
-  dice_rolls = 2
   dice_sum = 0
+  dice_rolls = int(input("How many dice?"))
+  dice_size = int(input("How many sides?"))
 
   for i in range(0,dice_rolls):
-    roll = random.randint(1,6)
+    roll = random.randint(1,dice_size)
     dice_sum += roll
-    print(f"You rolled a {roll}")
+    if roll == 1:
+      print(f"You rolled a {roll}! Critical fail!")
+    elif roll == dice_size:
+      print(f"A {roll}! Woop!")  
+    else:
+      print(f"You rolled a {roll}")
 
   print(f"You rolled a total of {dice_sum}")
 
